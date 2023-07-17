cf9cd27ce7fbfaa7813242b7732cc8da5a04f0ec
itsMayur <its.mayur718@gmail.com>
2023-07-17 01:06:23 -0700

    docs improvement and some backend fixes

--- backend/routes/GetBasket.js
+++ backend/routes/GetBasket.js
@@ -4,12 +4,19 @@ const Router = express.Router();
 
 Router.get("/GetBasket", (req, res) => {
   // CHECKING IS SESSION ID OF USER EXIST IN MONGOOSE DB
-  USER.exists({ user_id: req.session.sessionID }).then((response) => {
+  USER.exists({ user_id: req.body.sessionID }).then((response) => {
     // IF SESSION EXISTS
     if (Boolean(response)) {
       // FETCHING BASKET ITEAMS FROM DATABASE
-      BASKETITEM.find({ user_id: req.session.sessionID }).then((data) => {
-        res.json(data).status(200);
+      BASKETITEM.find({ user_id: req.body.sessionID }).then((data) => {
+        // IF NO ITEMS IN BASKET
+        if (data.length === 0) {
+          res.send({ message: "NO ITEMS IN CART" }).status(200);
+        }
+        // IF HAS SOME ITEMS IN BASKET
+        else {
+          res.send(data).status(200);
+        }
       });
     }
     // IF SESSION DOESN'T EXISTS
